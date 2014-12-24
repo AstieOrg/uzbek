f88849e3fb93b4792d8b9da6eb20b830a653e342
kartikaDS <dhitatracker@gmail.com>
2014-12-25 01:43:58 +0700

    Embed Video view

--- app/controller/media.php
+++ app/controller/media.php
@@ -73,8 +73,11 @@ class media extends Controller {
     	$_SESSION['lang'] = $lang;
     	$this->view->assign('language',$LANG);
 
-		//GET ALBUM
+		//GET VIDEO
 		$getVideo = $this->contentHelper->getVideo();
+		foreach ($getVideo as $key => $value) {
+			$getVideo[$key]['content_en'] = html_entity_decode($value['content_en'],ENT_QUOTES, 'UTF-8');
+		}
 		$this->view->assign('video',$getVideo);	
 		
 		return $this->loadView('video');
@@ -93,8 +96,12 @@ class media extends Controller {
     	$this->view->assign('language',$LANG);
 
 		$id = $_GET['id'];
+		
 		//VIEW VIDEO
 		$viewVideo = $this->contentHelper->viewVideo($id);
+		foreach ($viewVideo as $key => $value) {
+			$viewVideo[$key]['content_en'] = html_entity_decode($value['content_en'],ENT_QUOTES, 'UTF-8');
+		}
 		$this->view->assign('viewVideo',$viewVideo);	
 		
 		return $this->loadView('viewVideo');
